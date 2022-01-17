b126e3193d266263c4c6fee357e67f10399ffae3
Noel Schwabenland <noel@schwabenland.ch>
2022-01-17 23:14:40 +0100

    added source

--- device/main.py
+++ device/main.py
@@ -2,11 +2,17 @@ import RPi.GPIO as GPIO
 from time import sleep     # this lets us have a time delay (see line 15)
 
 # Example Code with some cleanup todo
+# Src: https://raspi.tv/2013/rpi-gpio-basics-6-using-inputs-and-outputs-together-with-rpi-gpio-pull-ups-and-pull-downs
 
+# Env
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
 GPIO.setup(25, GPIO.IN)    # set GPIO25 as input (button)
 GPIO.setup(24, GPIO.OUT)   # set GPIO24 as an output (LED)
 
+# Set initial input/outputs
+# @todo
+
+# Check for inputs
 try:
     while True:            # this will carry on until you hit CTRL+C
         if GPIO.input(25):  # if port 25 == 1
