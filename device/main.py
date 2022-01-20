bfbce141381607692a20ec81fdb0102bbe70bf90
Noel Schwabenland <noel@schwabenland.ch>
2022-01-20 14:07:11 +0100

    added json data

--- device/main.py
+++ device/main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep     # this lets us have a time delay (see line 15)
+import requests
 
 # Example Code with some cleanup todo
 # Src: https://raspi.tv/2013/rpi-gpio-basics-6-using-inputs-and-outputs-together-with-rpi-gpio-pull-ups-and-pull-downs
@@ -18,7 +19,7 @@ try:
         if GPIO.input(25):  # if port 25 == 1
             headers = {"charset": "utf-8", "Content-Type": "application/json"}
             url = 'http://192.168.0.14:3000/foo-bar'
-            r = requests.post(url, json=YOUR_JSON_DATA, headers=headers)
+            r = requests.post(url, json={'foo': 'bar'}, headers=headers)
             print(r.text)
             GPIO.output(24, 1)         # set port/pin value to 1/HIGH/True
         else:
